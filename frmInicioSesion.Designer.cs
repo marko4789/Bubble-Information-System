78f27126c9ae5dc35dee34c08aaf5465cefe2971
Marco Ocampos <markos4789@gmail.com>
2020-02-21 22:19:34 -0700

    1ra versión del inicio de sesión

--- frmInicioSesion.Designer.cs
+++ frmInicioSesion.Designer.cs
@@ -33,61 +33,77 @@
             this.labelContrasena = new System.Windows.Forms.Label();
             this.txtUsuario = new System.Windows.Forms.TextBox();
             this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.lbTitulo = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnIniciarSesion
             // 
-            this.btnIniciarSesion.Location = new System.Drawing.Point(55, 222);
+            this.btnIniciarSesion.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciarSesion.Location = new System.Drawing.Point(240, 201);
             this.btnIniciarSesion.Name = "btnIniciarSesion";
-            this.btnIniciarSesion.Size = new System.Drawing.Size(182, 53);
+            this.btnIniciarSesion.Size = new System.Drawing.Size(114, 27);
             this.btnIniciarSesion.TabIndex = 0;
             this.btnIniciarSesion.Text = "Iniciar sesion";
             this.btnIniciarSesion.UseVisualStyleBackColor = true;
+            this.btnIniciarSesion.Click += new System.EventHandler(this.btnIniciarSesion_Click);
             // 
             // labelUsuario
             // 
             this.labelUsuario.AutoSize = true;
-            this.labelUsuario.Location = new System.Drawing.Point(27, 61);
+            this.labelUsuario.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUsuario.Location = new System.Drawing.Point(97, 113);
             this.labelUsuario.Name = "labelUsuario";
-            this.labelUsuario.Size = new System.Drawing.Size(43, 13);
+            this.labelUsuario.Size = new System.Drawing.Size(68, 21);
             this.labelUsuario.TabIndex = 1;
             this.labelUsuario.Text = "Usuario";
             // 
             // labelContrasena
             // 
             this.labelContrasena.AutoSize = true;
-            this.labelContrasena.Location = new System.Drawing.Point(27, 126);
+            this.labelContrasena.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelContrasena.Location = new System.Drawing.Point(97, 160);
             this.labelContrasena.Name = "labelContrasena";
-            this.labelContrasena.Size = new System.Drawing.Size(61, 13);
+            this.labelContrasena.Size = new System.Drawing.Size(97, 21);
             this.labelContrasena.TabIndex = 2;
             this.labelContrasena.Text = "Contraseña";
             // 
             // txtUsuario
             // 
-            this.txtUsuario.Location = new System.Drawing.Point(137, 61);
+            this.txtUsuario.Location = new System.Drawing.Point(242, 116);
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.Size = new System.Drawing.Size(112, 20);
             this.txtUsuario.TabIndex = 3;
             // 
             // txtContrasena
             // 
-            this.txtContrasena.Location = new System.Drawing.Point(137, 126);
+            this.txtContrasena.Location = new System.Drawing.Point(242, 163);
             this.txtContrasena.Name = "txtContrasena";
-            this.txtContrasena.Size = new System.Drawing.Size(100, 20);
+            this.txtContrasena.Size = new System.Drawing.Size(112, 20);
             this.txtContrasena.TabIndex = 4;
+            // 
+            // lbTitulo
+            // 
+            this.lbTitulo.AutoSize = true;
+            this.lbTitulo.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitulo.Location = new System.Drawing.Point(110, 27);
+            this.lbTitulo.Name = "lbTitulo";
+            this.lbTitulo.Size = new System.Drawing.Size(226, 36);
+            this.lbTitulo.TabIndex = 5;
+            this.lbTitulo.Text = "Inicio de sesión";
             // 
             // FrmInicioSesion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(293, 325);
+            this.ClientSize = new System.Drawing.Size(434, 273);
+            this.Controls.Add(this.lbTitulo);
             this.Controls.Add(this.txtContrasena);
             this.Controls.Add(this.txtUsuario);
             this.Controls.Add(this.labelContrasena);
             this.Controls.Add(this.labelUsuario);
             this.Controls.Add(this.btnIniciarSesion);
             this.Name = "FrmInicioSesion";
-            this.Text = "Inicio de Sesion";
+            this.Text = "Bubble System Information";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,6 +116,7 @@
         private System.Windows.Forms.Label labelContrasena;
         private System.Windows.Forms.TextBox txtUsuario;
         private System.Windows.Forms.TextBox txtContrasena;
+        private System.Windows.Forms.Label lbTitulo;
     }
 }
 
--- frmInicioSesion.cs
+++ frmInicioSesion.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,96 @@ namespace Bubble_Information_System
 {
     public partial class FrmInicioSesion : Form
     {
+
+        private MySqlConnection     conexionDB;
+        private MySqlCommand        sqlComando;
+        private MySqlDataAdapter    adaptador;
+        private MySqlDataReader     registro;
         public FrmInicioSesion()
         {
             InitializeComponent();
+
+            MySqlConnectionStringBuilder strConstructor = new MySqlConnectionStringBuilder();
+
+            strConstructor.Server = "localhost";
+            strConstructor.UserID = "root";
+            strConstructor.Password = "";
+            strConstructor.Database = "dbLavanderia";
+
+            try
+            {
+                this.conexionDB = new MySqlConnection(strConstructor.ToString());
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+
         }
+
+        private void btnIniciarSesion_Click(object sender, EventArgs e)
+        {
+            String usuario = this.txtUsuario.Text;
+            String password = this.txtContrasena.Text;
+
+            if ( ( usuario != "" ) && ( password != "" ))
+            {
+                logear(usuario, password);
+            }
+            else
+            {
+                MessageBox.Show("Porfavor rellene ambos campos", "Bubble Information System");
+            }
+
+        }
+
+
+        public void logear(string nombreUsuario, string contraseña)
+        {
+            try
+            {
+                conexionDB.Open();
+                sqlComando = new MySqlCommand("select nombreUsuario, tipoUsuario from usuario where nombreUsuario =?nombreUsuario and contraseña =?contraseña and status = 0 ", conexionDB);
+                sqlComando.Parameters.AddWithValue("nombreUsuario", nombreUsuario);
+                sqlComando.Parameters.AddWithValue("contraseña", contraseña);
+                
+                adaptador = new MySqlDataAdapter(sqlComando);
+                DataTable dt = new DataTable();
+                adaptador.Fill(dt);
+                if (dt.Rows.Count == 1)
+                {
+                    this.Hide();
+                    if (dt.Rows[0][1].ToString() == "Administrador")
+                    {
+                        //frmMenu me = new frmMenu();
+                        //me.Show();
+                        MessageBox.Show("Inicio de sesión como un Administrador", "Bubble Information System");
+                    }
+                    else if (dt.Rows[0][1].ToString() == "Usuario")
+                    {
+                        // frmMenuUsu meu = new frmMenuUsu();
+                        //meu.Show();
+                        MessageBox.Show("Inicio de sesión como un Usuario", "Bubble Information System");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("El usuario y/o contraseña es incorrecto", "Bubble Information System");
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            finally
+            {
+                conexionDB.Close();
+            }
+        }
+
+
+
+
+
     }
 }
